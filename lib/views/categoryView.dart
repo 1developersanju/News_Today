a7ea6dfaf16d0a237df41013318cd2b89c343aee
s4njeev <s4njeev.av@gmail.com>
2021-08-01 01:35:13 +0530

    new release | News Today

--- lib/views/categoryView.dart
+++ lib/views/categoryView.dart
@@ -3,6 +3,7 @@ import 'package:news_app_api/helper/outside_india/catagory_news_outside_india.da
 import 'package:news_app_api/models/categorie_model.dart';
 import 'package:news_app_api/helper/data.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 
 class CategoryList extends StatefulWidget {
   CategoryList({Key key}) : super(key: key);
@@ -27,32 +28,37 @@ class _CategoryListState extends State<CategoryList> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                /// Categories
-                // "Global".text.size(15).yellow400.makeCentered(),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  height: MediaQuery.of(context).size.height,
-                  child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                          maxCrossAxisExtent: 200,
-                          childAspectRatio: 3 / 2,
-                          crossAxisSpacing: 20,
-                          mainAxisSpacing: 20),
-                      addAutomaticKeepAlives: false,
-                      itemCount: categories.length,
-                      itemBuilder: (context, index) {
-                        return CategoryCard(
-                          imageAssetUrl: categories[index].imageAssetUrl,
-                          categoryName: categories[index].categorieName,
-                        );
-                      }),
-                ),
-              ],
+      body: DoubleBackToCloseApp(
+        snackBar: const SnackBar(
+          content: Text('Tap back again to leave'),
+        ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  /// Categories
+                  // "Global".text.size(15).yellow400.makeCentered(),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    height: MediaQuery.of(context).size.height,
+                    child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 200,
+                            childAspectRatio: 3 / 2,
+                            crossAxisSpacing: 20,
+                            mainAxisSpacing: 20),
+                        addAutomaticKeepAlives: false,
+                        itemCount: categories.length,
+                        itemBuilder: (context, index) {
+                          return CategoryCard(
+                            imageAssetUrl: categories[index].imageAssetUrl,
+                            categoryName: categories[index].categorieName,
+                          );
+                        }),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
